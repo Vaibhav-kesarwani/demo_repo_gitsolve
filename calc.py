5d99eac58996e74776596184f7cdabd0871001cd
Parul <parul.srivastava.2004@gmail.com>
2025-12-11 21:27:44 +0530

    first commit

---
+++ calc.py
@@ -0,0 +1,11 @@
+def add(x, y):
+    return x + y
+
+def main():
+    a = 5
+    b = 10
+    result = add(a, b)
+    print(f"The sum of {a} and {b} is {result}")
+
+if __name__ == "__main__":
+    main()
